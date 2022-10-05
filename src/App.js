8e05d6225a31428971532cd232bf584ebd69e42f
Pontus Kronenberg <a21ponkr@student.his.se>
2022-10-05 21:48:23 +0200

    react + web socket, yahoo finance

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import protobuf from 'protobufjs';
+const { Buffer } = require('buffer/');
+
+function formatPrice(price) {
+  return `$${price.toFixed(2)}`;
+}
 
 function App() {
+
+  const [stock, setStock] = useState(null);
+
+  useEffect(() => {
+    const ws = new WebSocket('wss://streamer.finance.yahoo.com');
+    protobuf.load('./PricingData.proto', (error, root) => {
+      if(error) {
+        return console.log(error);
+      }
+      const Ticker = root.lookupType('ticker');
+      ws.onopen = function open() {
+        console.log('connected');
+        ws.send(JSON.stringify({
+          subscribe: ['AMD']
+        }));
+      };
+      
+      ws.onclose = function close() {
+        console.log('disconnected');
+      };
+      
+      ws.onmessage = function incoming(message) {
+        const next = Ticker.decode(new Buffer(message.data, 'base64'));
+        setStock(next);
+      };
+    
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>STOCK PRICE</h1>
+      {stock && <h2>{formatPrice(stock.price)} USD</h2>}
     </div>
   );
 }
